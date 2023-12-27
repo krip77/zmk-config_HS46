43542a9b6bad216b5b96ae117c8b45e505567756
krip77 <99188264+krip77@users.noreply.github.com>
2023-12-27 10:51:21 +0100

    Add files via upload

---
+++ config/keymap_KP_swe.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "keycodes.h"
+// clang-format off
+
+// Basic keys
+#define SE_SECT &kp GRAVE  // §
+#define SE_1    &kp N1     // 1
+#define SE_2    &kp N2     // 2
+#define SE_3    &kp N3     // 3
+#define SE_4    &kp N4     // 4
+#define SE_5    &kp N5     // 5
+#define SE_6    &kp N6     // 6
+#define SE_7    &kp N7     // 7
+#define SE_8    &kp N8     // 8
+#define SE_9    &kp N9     // 9
+#define SE_0    &kp N0     // 0
+#define SE_PLUS &kp MINUS  // +
+#define SE_ACUT &kp EQUAL  // ´ (dead)
+#define SE_Q    &kp Q      // Q
+#define SE_W    &kp W      // W
+#define SE_E    &kp E      // E
+#define SE_R    &kp R      // R
+#define SE_T    &kp T      // T
+#define SE_Y    &kp Y      // Y
+#define SE_U    &kp U      // U
+#define SE_I    &kp I      // I
+#define SE_O    &kp O      // O
+#define SE_P    &kp P      // P
+#define SE_ARNG &kp LBRACKET // Å
+#define SE_DIAE &kp RBRACKET // ¨ (dead)
+#define SE_A    &kp A      // A
+#define SE_S    &kp S      // S
+#define SE_D    &kp D      // D
+#define SE_F    &kp F      // F
+#define SE_G    &kp G      // G
+#define SE_H    &kp H      // H
+#define SE_J    &kp J      // J
+#define SE_K    &kp K      // K
+#define SE_L    &kp L      // L
+#define SE_ODIA &kp SEMI   // Ö
+#define SE_ADIA &kp SQT    // Ä
+#define SE_QUOT &kp NUHS   // '
+#define SE_LABK &kp NUBS   // <
+#define SE_Z    &kp Z      // Z
+#define SE_X    &kp X      // X
+#define SE_C    &kp C      // C
+#define SE_V    &kp V      // V
+#define SE_B    &kp B      // B
+#define SE_N    &kp N      // N
+#define SE_M    &kp M      // M
+#define SE_COMM &kp COMMA  // ,
+#define SE_DOT  &kp DOT    // .
+#define SE_MINS &kp SLASH  // -
+
+// Shifted keys
+#define SE_HALF &skp GRAVE   // ½
+#define SE_EXLM &skp N1      // !
+#define SE_DQUO &skp N2      // "
+#define SE_HASH &skp N3      // #
+#define SE_CURR &skp N4      // ¤
+#define SE_PERC &skp N5      // %
+#define SE_AMPR &skp N6      // &
+#define SE_SLSH &skp N7      // /
+#define SE_LPRN &skp N8      // (
+#define SE_RPRN &skp N9      // )
+#define SE_EQL  &skp N0      // =
+#define SE_QUES &skp MINUS   // ?
+#define SE_GRV  &skp EQUAL   // ` (dead)
+#define SE_CIRC &skp RBRACKET// ^ (dead)
+#define SE_ASTR &skp NUHS    // *
+#define SE_RABK &skp NUBS    // >
+#define SE_SCLN &skp COMMA   // ;
+#define SE_COLN &skp DOT     // :
+#define SE_UNDS &skp SLASH   // _
+
+// AltGr (Right Alt) keys
+#define SE_AT   &akp N2      // @
+#define SE_PND  &akp N3      // £
+#define SE_DLR  &akp N4      // $
+#define SE_EURO &akp N5      // €
+#define SE_LCBR &akp N7      // {
+#define SE_LBRC &akp N8      // [
+#define SE_RBRC &akp N9      // ]
+#define SE_RCBR &akp N0      // }
+#define SE_BSLS &akp MINUS   // \
+#define SE_TILD &akp RBRACKET//
